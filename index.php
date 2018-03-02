8345cbb38aa034a36a452c690728b571d95f725f
Oscar Nilsson <oscar@oncolour.se>
2018-03-02 09:36:09 +0100

    adding all start files

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+    $your_meta_title = "Stressbird – Coming soon";
+$your_meta_description = "Ett verktyg för att dokumentera och hantera din stress";
+require 'includes/page_header.php'; ?>
+
+
+<main>
+
+</main>
+
+
+<?php require 'includes/page_footer.php' ?>
